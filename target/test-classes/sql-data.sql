e68cad1c0eba938bb7ef2e99865f47bf37962523
Mrcwl1996 <71522034+Mrcwl1996@users.noreply.github.com>
2020-11-27 12:50:00 +0000

    Add files via upload

---
+++ target/test-classes/sql-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers (first_name, surname) 
+VALUES ('William', 'Chu');
+
+INSERT INTO customers (first_name, surname) 
+VALUES ('Chris', 'Man');
+
+INSERT INTO items (name, value)
+VALUES ('PS1', '100');
+
+INSERT INTO items (name, value)
+VALUES ('PS2', '200');
+
+INSERT INTO items (name, value)
+VALUES ('PS3', '300');
