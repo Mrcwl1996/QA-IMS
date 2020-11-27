af5adaf2c108d75004a04e2a43be6a02b2eac1ac
Mrcwl1996 <71522034+Mrcwl1996@users.noreply.github.com>
2020-11-27 12:40:36 +0000

    Create DAOTest.java

---
+++ src/test/java/com/qa/ims/persistence/dao/DAOTest.java
@@ -0,0 +1,14 @@
+//package com.qa.ims.persistence.dao;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class DAOTest {
+//
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
